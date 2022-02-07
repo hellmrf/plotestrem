43e272963eceebfd2b524a39e4169c8ad340446c
Heliton Martins <helitonmrf@gmail.com>
2022-02-07 17:09:32 -0300

    :bookmark:  Bump VERSION to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plotestrem",
-    version="0.0.1",
+    version="0.0.2",
     author="Heliton Martins Reis Filho",
     author_email="helitonmrf@gmail.com",
     license="GPL-v3.0",
